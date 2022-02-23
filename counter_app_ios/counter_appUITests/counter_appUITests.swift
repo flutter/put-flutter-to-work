f8a8c8ad1fc3e00518707c273649c36414566a5c
Jan-Stepien <jan.stepien@widgeters.dev>
2022-02-23 16:41:48 +0100

    Update counter_app_ios/counter_appUITests/counter_appUITests.swift
    Co-authored-by: Felix Angelov <felangelov@gmail.com>

--- counter_app_ios/counter_appUITests/counter_appUITests.swift
+++ counter_app_ios/counter_appUITests/counter_appUITests.swift
@@ -7,7 +7,7 @@
 
 import XCTest
 
-class counter_appUITests: XCTestCase {
+class CounterAppUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
