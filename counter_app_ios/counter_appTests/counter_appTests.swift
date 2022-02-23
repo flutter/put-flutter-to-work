b8c2c07755be9888c05fe44f2d489064cce75766
Jan-Stepien <jan.stepien@widgeters.dev>
2022-02-23 16:41:53 +0100

    Update counter_app_ios/counter_appTests/counter_appTests.swift
    Co-authored-by: Felix Angelov <felangelov@gmail.com>

--- counter_app_ios/counter_appTests/counter_appTests.swift
+++ counter_app_ios/counter_appTests/counter_appTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import counter_app
 
-class counter_appTests: XCTestCase {
+class CounterAppTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
