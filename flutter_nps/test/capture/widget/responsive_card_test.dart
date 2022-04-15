cd5a7f4f43e33d2bf64a8e864a6949f8d02ad3ee
Jan Stepien <jasiek.stepien@gmail.com>
2022-04-15 15:31:29 +0200

    fix flutter_nps tests

--- flutter_nps/test/capture/widget/responsive_card_test.dart
+++ flutter_nps/test/capture/widget/responsive_card_test.dart
@@ -1,6 +1,5 @@
+import 'package:app_ui/app_ui.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_nps/breakpoints.dart';
-import '../../../packages/app_ui/lib/src/widgets/responsive_card.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../../helpers/helpers.dart';
