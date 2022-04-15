ad949e9a2166a740efa3055335ad7c36b7551439
Jan Stepien <jasiek.stepien@gmail.com>
2022-04-15 15:26:49 +0200

    rename styles to text styles

--- flutter_nps/packages/app_ui/lib/app_ui.dart
+++ flutter_nps/packages/app_ui/lib/app_ui.dart
@@ -5,7 +5,7 @@ export 'src/generated/assets.gen.dart';
 export 'src/spacing/breakpoints.dart';
 export 'src/spacing/spacing.dart';
 export 'src/theme/app_theme.dart';
-export 'src/typography/styles.dart';
+export 'src/typography/text_styles.dart';
 export 'src/widgets/answer_chips.dart';
 export 'src/widgets/capture_score_selector.dart';
 export 'src/widgets/capture_score_selector_labels.dart';
