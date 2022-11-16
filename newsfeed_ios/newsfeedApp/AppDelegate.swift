7515c37309cc839d24d969bf2f677bb897c49311
Huan Lin <hellohuanlin@gmail.com>
2022-11-16 12:24:17 -0800

    make app delegate to conform to flutter app lifecycle provider

--- newsfeed_ios/newsfeedApp/AppDelegate.swift
+++ newsfeed_ios/newsfeedApp/AppDelegate.swift
@@ -6,11 +6,54 @@
 import Foundation
 import Flutter
 
-class AppDelegate: FlutterAppDelegate, ObservableObject {
+class AppDelegate: UIResponder, UIApplicationDelegate, FlutterAppLifeCycleProvider, ObservableObject {
+
+  private let lifecycleDelegate = FlutterPluginAppLifeCycleDelegate()
+
   let npsflutterEngine = FlutterEngine(name: "flutter_nps_engine")
 
-  override func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
     npsflutterEngine.run()
-    return true
+    return lifecycleDelegate.application(application, didFinishLaunchingWithOptions: launchOptions ?? [:])
+  }
+
+  func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+    lifecycleDelegate.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+  }
+
+  func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+    lifecycleDelegate.application(application, didFailToRegisterForRemoteNotificationsWithError: error)
+  }
+
+  func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable : Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+    lifecycleDelegate.application(application, didReceiveRemoteNotification: userInfo, fetchCompletionHandler: completionHandler)
+  }
+
+  func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+    return lifecycleDelegate.application(app, open: url, options: options)
+  }
+
+  func application(_ application: UIApplication, handleOpen url: URL) -> Bool {
+    return lifecycleDelegate.application(application, handleOpen: url)
+  }
+
+  func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+    return lifecycleDelegate.application(application, open: url, sourceApplication: sourceApplication ?? "", annotation: annotation)
+  }
+
+  func application(_ application: UIApplication, performActionFor shortcutItem: UIApplicationShortcutItem, completionHandler: @escaping (Bool) -> Void) {
+    lifecycleDelegate.application(application, performActionFor: shortcutItem, completionHandler: completionHandler)
+  }
+
+  func application(_ application: UIApplication, handleEventsForBackgroundURLSession identifier: String, completionHandler: @escaping () -> Void) {
+    lifecycleDelegate.application(application, handleEventsForBackgroundURLSession: identifier, completionHandler: completionHandler)
+  }
+
+  func application(_ application: UIApplication, performFetchWithCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+    lifecycleDelegate.application(application, performFetchWithCompletionHandler: completionHandler)
+  }
+
+  func add(_ delegate: FlutterApplicationLifeCycleDelegate) {
+    lifecycleDelegate.add(delegate)
   }
 }
