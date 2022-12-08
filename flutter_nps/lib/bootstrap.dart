84d138efb0e643e95d4b41939304c8aa6a1152dc
Brett Morgan <brett.morgan@gmail.com>
2022-12-08 11:50:01 +1100

    Tidy up (#170)

--- flutter_nps/lib/bootstrap.dart
+++ flutter_nps/lib/bootstrap.dart
@@ -23,13 +23,5 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
     log(details.exceptionAsString(), stackTrace: details.stack);
   };
 
-  await runZonedGuarded(
-    () async {
-      await BlocOverrides.runZoned(
-        () async => runApp(await builder()),
-        blocObserver: AppBlocObserver(),
-      );
-    },
-    (error, stackTrace) => log(error.toString(), stackTrace: stackTrace),
-  );
+  Bloc.observer = AppBlocObserver();
 }
